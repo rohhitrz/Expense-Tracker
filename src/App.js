1234a2d17e6ed7ab1567204612286c9fc282e513
Rohit <rohhit.rz@gmail.com>
2024-08-28 23:34:25 +0530

    AddBalanceForm added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import ExpenseForm from "./components/Forms/ExpenseForm/ExpenseForm";
+import AddBalanceForm from "./components/Forms/AddBalanceForm/AddBalanceForm";
 
 function App() {
   return (
     <div className="App">
     <ExpenseForm/>
+    <AddBalanceForm/>
     </div>
   );
 }
--- src/components/Forms/AddBalanceForm/AddBalanceForm.jsx
+++ src/components/Forms/AddBalanceForm/AddBalanceForm.jsx
@@ -1,52 +1,46 @@
 import { useState } from "react";
 import { useSnackbar } from "notistack";
 import Button from "../../Button/Button";
+import styles from "./AddBalanceForm.module.css"
 
-function AddBalanceForm({setIsOpen,setBalance}){
+function AddBalanceForm({ setIsOpen, setBalance }) {
+  const [income, setIncome] = useState("");
+  const { enqueSnackbar } = useSnackbar();
 
-    const [income,setIncome]=useState('');
-    const {enqueSnackbar}=useSnackbar();
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
+    if (Number(income) < 0) {
+      enqueSnackbar("Income should be more than 0", { variant: "warning" });
+      setIsOpen(false);
+      return;
+    }
+    setBalance((prev) => prev + Number(income));
+    setIsOpen(false);
+  };
 
-    const handleSubmit=(e)=>{
-        e.preventDefault();
+  return (
+    <div className={styles.formWrapper}>
+      <h3>Add Balance</h3>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="number"
+          placeholder="Income Amount"
+          value={income}
+          onChange={(e) => setIncome(e.target.value)}
+          required
+        />
 
-        if(Number(income)<0){
-            enqueSnackbar("Income should be more than 0", {variant:"warning"});
-            setIsOpen(false);
-            return;
-        }
-        setBalance((prev)=>prev+ Number(income));
-        setIsOpen(false);
-}
+        <Button type="submit" btnstyle="primary" shadow>
+          Add Balance
+        </Button>
 
-return(
-
-    <div>
-
-        <h3>Add Balance</h3>
-        <form onSubmit={handleSubmit}>
-            <input 
-            type="number" 
-            placeholder="Income Amount"
-            value={income}
-            onChange={(e)=>setIncome(e.target.value)}
-            required
-         />
-
-         <Button type="submit" btnstyle="primary" shadow>Add Balance</Button>
-
-         <Button>
-            
-         </Button>
-
-
-
-        </form>
-
+        <Button style="secondary" shadow handleClick={() => setIsOpen(false)}>
+          Cancel
+        </Button>
+      </form>
     </div>
-)
-
-
-
+  );
 }
+
+export default AddBalanceForm;
