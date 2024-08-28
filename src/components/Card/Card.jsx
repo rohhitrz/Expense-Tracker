a231ab16b77520d1d52d8ba19ce412006c09da20
Rohit <rohhit.rz@gmail.com>
2024-08-29 01:14:20 +0530

    card component added

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,25 @@
+import Button from "../Button/Button";
+import styles from "./Card.module.css";
+
+function Card({
+  title,
+  money,
+  buttonText,
+  buttonType,
+  handleclick,
+  success = True,
+}) {
+  return (
+    <div className={styles.card}>
+      <h3 className={styles.title}>
+        {`${title}: `}
+        <span
+          className={success ? styles.success : styles.failure}
+        >{`${money}`}</span>
+      </h3>
+      <Button handleClick={handleclick} style={buttonType}>
+        {buttonText}
+      </Button>
+    </div>
+  );
+}
